f16c600a26fe0017e3b2916a24a75d38894c7b1c
AmanJariwala <60352651+AmanJariwala@users.noreply.github.com>
2021-06-10 18:26:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -192,11 +192,191 @@ button[type=submit]:hover {
         print("Error: Data not done;")
         return "Good you signed up! Now go here back for sign-in:  " + "<a href = \"/\">Click here :)</a>"
 
+@app.route('/recinvite')
+def recinvite():
+    cursor.execute("Select email from currentemail;")
+    recemail = cursor.fetchone()
+    recemail = str(recemail[0])
+
+    cursor.execute("Select event_name from invitation where email = '" + recemail + "' ;")
+    eventsss = cursor.fetchall()    # check from invitation table if email matches the login email then print the event details and and give accept and decline option
+
+    names = []
+    types = []
+    locations = []
+    dates = []
+    details = []
+    orgzzz = []
+    for event in eventsss:
+        query = "select * from events where name='" + event[0] + "';"
+        # query = "select ev.event_id, ev.event_name, ev.event_type, ev.location, ev.date_time from Event ev join Feed fd on ev.event_id = fd.event_id where fd.user_email = %s;"
+        cursor.execute(query)
+        # ev_data = str(cursor.fetchall())
+
+        (event_name, event_date, location, event_type, detail, organizer) = cursor.fetchall()[0]
+        names.append(event_name)
+        types.append(event_type)
+        locations.append(location)
+        dates.append(event_date)
+        details.append(detail)
+        orgzzz.append(organizer)
+
+    aman = """
+        <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <style>
+    h1{
+      font-family:noteworthy;
+    }
+    h2 {
+
+      padding-left: 200px;
+      font-family:noteworthy;
+      text-decoration: underline;
+    }
+
+    h3{
+      text-decoration: underline;
+    }
+    textarea {
+      padding-left: 100px;
+      font-size: large;
+    }
+    body {
+      background-color: #ff9999
+    }
+    input[type=submit] {
+      width: 50%;
+      background-color: #000000;
+      color: white;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: none;
+      border-radius: 4px;
+      cursor: pointer;
+    }
+    textarea{
+      width: 20%;
+      /*padding: 14px 20px;*/
+      margin: 15px 0;
+      border: none;
+      border-radius: 4px;
+    }
+
+    input[type=submit]:hover {
+      background-color: #000000;
+    }
+    div.lalala {
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+      color: white;
+      font-weight: bold;
+      border: 3px solid #f1f1f1;
+      z-index: 2;
+      width: 70%;
+      padding: 40px;
+      float: left;
+    }
+
+    form {
+      background-color: rgb(0,0,0); /* Fallback color */
+      background-color: rgba(0,0,0, 0.4); /* Black w/opacity/see-through */
+      color: white;
+      font-weight: bold;
+      border: 3px solid #f1f1f1;
+      z-index: 2;
+      width: 40%;
+      padding: 40px;
+      float: left;
+    }
+    </style>
+        <center><h1>EVENT MANAGER</h1></center>
+    </head>
+    <body>
+    <div>
+      <div style="width: 100%">
+        <img src="/static/PP3.jpeg" style="float:right;" width="750" height="650">
+      </div>
+      <div >
+        <form>
+        <h2>Invitations Received</h2>  
+        """
+    for i in range(len(names)):
+            # aman += "<div class= 'lalala'>" + "Event_Name: " + names[i] + "</br>" + dates[i] + "</br>" + locations[i] + "</br>" + types[i] + "</br>" + details[i] + "</br>" + orgzzz[i] + "</br>" + "<a href='/invites'>Send Invitation</a>""</div>"
+            aman += "<div class= 'lalala'>" + "Event_Name: " + names[i] + "</br>" + dates[i] + "</br>" + locations[i]
+            aman += "</br>" + types[i] + "</br>" + details[i] + "</br>" + orgzzz[i] + "</br>"
+            aman += "<a href='/accept?ev_name="+ names[i] +"'>Accept invitation</a></br>"
+            aman += "<a href='/decline?ev_name=" + names[i] + "'>Decline invitation</a></div>"
+    aman += """
+        <input type="submit" value="Go to Profile" formaction = "/Profile">
+        </div>
+
+
+      </form>
+    </div>
+    </body>
+    </html>
+        """
+    return aman
+
+@app.route('/accept')
+def accept_invite():
+    cursor.execute("Select email from currentemail;")
+    recemail = cursor.fetchone()
+    recemail = str(recemail[0])
+
+    ev = request.args.get('ev_name', None)
+    print("accepted " + str(ev) + " invitation!!!")
+
+    query = "select * from events where name='" + ev + "';"
+    # query = "select ev.event_id, ev.event_name, ev.event_type, ev.location, ev.date_time from Event ev join Feed fd on ev.event_id = fd.event_id where fd.user_email = %s;"
+    cursor.execute(query)
+    # ev_data = str(cursor.fetchall())
+
+    names = []
+    types = []
+    locations = []
+    dates = []
+    details = []
+    orgzzz = []
+    for (event_name, event_date, location, event_type, detail, organizer) in cursor:
+        names.append(event_name)
+        types.append(event_type)
+        locations.append(location)
+        dates.append(event_date)
+        details.append(detail)
+        orgzzz.append(organizer)
+
+    cursor.execute("INSERT INTO events (name, date, location, type, details, organizer) values ('" + names[0] + "', '" + dates[0] + "', '" + locations[0] + "', '" + types[0] + "', '" + details[0] + "', '" + recemail + "');")
+    cnx.commit()
+
+    cursor.execute("Delete from invitation where event_name='" + ev + "' and email='" + recemail + "';")
+    cnx.commit()
+
+    return recinvite()
+
+@app.route('/decline')
+def decline_invite():
+    cursor.execute("Select email from currentemail;")
+    recemail = cursor.fetchone()
+    recemail = str(recemail[0])
+
+    ev = request.args.get('ev_name', None)
+    print("declined " + str(ev) + " invitation!!!")
+    cursor.execute("Delete from invitation where event_name='" + ev + "' and email='"+ recemail +"';")
+    cnx.commit()
+    return recinvite()
 
 
 
-
-
+  
+  
+  
+  
+  
+  
+  
 # @app.route('/', methods=["GET", "POST"])
 # def signinn():
 #     if request.method == "POST":
